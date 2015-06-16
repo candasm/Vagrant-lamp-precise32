8291681b13504f32f2a6fb093a4f4760816358f2
Candas Minareci <candasm@gmail.com>
2015-06-16 22:18:19 +0300

    apache yuklemesi mod rewrite yuklemesi eklendi

--- provisions/apache.sh
+++ provisions/apache.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+echo '========================================================='
+echo "Apache2.4 Install"
+
+apt-get install -y apache2
+a2enmod ssl rewrite
+
+#todo buraya self signed ssl gelecek
+
+rm -rf /var/www
+ln -fs /vagrant/public /var/www
+
+sudo sed -i '/export APACHE_RUN_USER=www-data/c export APACHE_RUN_USER=vagrant' /etc/apache2/envvars
+sudo sed -i '/export APACHE_RUN_GROUP=www-data/c export APACHE_RUN_GROUP=vagrant' /etc/apache2/envvars
