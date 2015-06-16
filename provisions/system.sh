12846b32f496bc34f1085f99975c0588a694db2c
Candas Minareci <candasm@gmail.com>
2015-06-16 20:20:43 +0300

    ondrej/php5 repository added

--- provisions/system.sh
+++ provisions/system.sh
@@ -6,6 +6,8 @@ echo '========================================================='
 echo "system update..."
     echo "Europe/Istanbul" | sudo tee /etc/timezone
     dpkg-reconfigure --frontend noninteractive tzdata
+    echo "ondrej/php5 repository added"
+    add-apt-repository ppa:ondrej/php5 -y
     apt-get update
     locale-gen tr_TR.UTF-8
 	export LC_CTYPE="tr_TR.UTF-8"
