87742a4019b8a42f8aae87204d308b788cc0db6f
Candas Minareci <candasm@gmail.com>
2015-06-16 21:57:08 +0300

    repository duzenlemesi yapildi. Vagrant icine shell prov yuklemesi eklendi

--- provisions/system.sh
+++ provisions/system.sh
@@ -6,11 +6,11 @@ echo '========================================================='
 echo "system update..."
     echo "Europe/Istanbul" | sudo tee /etc/timezone
     dpkg-reconfigure --frontend noninteractive tzdata
-    echo "ondrej/php5 repository added"
-    add-apt-repository ppa:ondrej/php5 -y
     apt-get update
     locale-gen tr_TR.UTF-8
 	export LC_CTYPE="tr_TR.UTF-8"
 	echo 'export LC_CTYPE="tr_TR.UTF-8"' >> ~/.bash_profile
 	apt-get install -y build-essential python-software-properties curl vim git
+    add-apt-repository ppa:ondrej/php5 -y
+    apt-get update
 echo '========================================================='
