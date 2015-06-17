c061b0fe692e5e1b0966fb8ab8f9a0d55f95564b
Candas Minareci <candasm@gmail.com>
2015-06-17 19:59:21 +0300

    mysql kurulumu yazildi

--- provisions/mysql.sh
+++ provisions/mysql.sh
@@ -3,3 +3,12 @@
 echo '========================================================='
 echo ">> Mysql database server will start to install"
 
+debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password root"
+
+apt-get install -y mysql-server --fix-missing --fix-broken
+echo ">> mysql server yuklemesi tamamlandi"
+apt-get install -y php5-mysql
+echo ">> php5 mysql paketi yuklendi"
+echo "create database default_database CHARACTER SET utf8 COLLATE utf8_general_ci" | mysql -uroot -proot
+echo ">> mysql default database olusturuldu"
