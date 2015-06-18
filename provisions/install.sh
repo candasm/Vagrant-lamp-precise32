f9faec0cef42b2f57e420748b3f4b7c1bc5cd0a9
Candas Minareci <candasm@gmail.com>
2015-06-19 01:51:37 +0300

    apache ve mysql servislerinin kurulum sonrasi ve acilista yeniden baslatilmasi eklendi.

--- provisions/install.sh
+++ provisions/install.sh
@@ -21,8 +21,18 @@ then
     bash ./php5.sh
     touch "$systemLogRoot/phpSetup"
 fi
+
 if [ ! -f "$systemLogRoot/mysqlSetup" ];
 then
     bash ./mysql.sh
     touch "$systemLogRoot/mysqlSetup"
+fi
+
+if [ -f /var/log/apacheSetup ];
+then
+	service apache2 restart
+fi
+if [ -f /var/log/mysqlSetup ];
+then
+	service mysql restart
 fi
